ae49a4a379428dcfd403eeec3db9c6bf800711e8
Sreyaea <sreya.ea@gmail.com>
2023-06-15 16:51:31 +0530

    create post modified

--- Community/client/src/App.js
+++ Community/client/src/App.js
@@ -58,7 +58,7 @@ function App() {
                 </>
               ) : (
                 <>
-                  <Link to="/community"> Community</Link>
+                  <Link to="/"> Community</Link>
                 </>
               )}
             </div>
